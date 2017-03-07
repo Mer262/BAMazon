5af6d7fd8fa079005e9022d09dc7fac89062abe7
Meredith Moore <meredithmoore@Merediths-MacBook-Pro.local>
2017-03-06 22:54:59 -0500

    pushing changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,34 +20,46 @@ connection.connect(function(err) {
 
 
 connection.query("SELECT * FROM products", function(err, res) {
+    console.log("Item ID | Product Name | Department Name | Price | Quantity Available")
     for (var i = 0; i < res.length; i++) {
         console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
     }
     console.log("-----------------------------------");
 });
 
-var start = function() {
-    inquirer.prompt({
-        name: "itemPick",
-        type: "input",
-        message: "Please enter the Item ID of the product you would like to buy.",
-        validate: function(value) {
-            if (isNaN(value) === false) {
-                return true;
-            }
-            return false;
-        }
-    }, {
-        name: "quantity",
-        type: "input",
-        message: "How many would you like to buy?",
-        validate: function(value) {
-            if (isNaN(value) === false) {
-                return true;
-            }
-            return false;
-        }
-    }).then(function(answer) {
-        console.log("ran without errors")
-    });
+function ItemToBuy(itemPick, quantity) {
+    this.itemPick = itemPick;
+    this.quanity = quantity;
+
+    this.printInfo = function() {
+        console.log("You picked Item ID " + this.itemPick + "\n, and you would like to buy " + this.quantity +
+            " of them.");
+    };
 };
+
+inquirer.prompt({
+    name: "itemPick",
+    // type: "input",
+    message: "Please enter the Item ID of the product you would like to buy."
+        // validate: function(value) {
+        //     if (isNaN(value) === false) {
+        //         return true;
+        //     }
+        //     return false;
+        // }
+}, {
+    name: "quantity",
+    // type: "input",
+    message: "How many would you like to buy?"
+        // validate: function(value) {
+        //     if (isNaN(value) === false) {
+        //         return true;
+        //     }
+        //     return false;
+        // }
+}).then(function(answers) {
+    var newItemsToBuy = new ItemToBuy(answers.itemPick, answers.quantity);
+    // printInfo method is run to show that the newguy object was successfully created and filled
+    newItemsToBuy.printInfo();
+    console.log("ran without errors")
+});
